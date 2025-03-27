5f43de94819ba3a7daaf0d0df0988fe8a86f13a7
Ashish <ashishredhu5810@gmail.com>
2025-03-28 00:46:22 +0530

    dark mode enabled

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),  tailwindcss(),],
+  darkMode: "class", // Force dark mode using a class
   server:{
     host:'0.0.0.0',
     port: 5173,
